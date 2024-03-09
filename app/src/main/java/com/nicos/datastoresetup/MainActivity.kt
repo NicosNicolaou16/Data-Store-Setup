af6360f4c7952e55426ceadcf2289b3eb9deeae3
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-03-09 22:12:38 +0200

    add library, and build the UI

--- app/src/main/java/com/nicos/datastoresetup/MainActivity.kt
+++ app/src/main/java/com/nicos/datastoresetup/MainActivity.kt
@@ -3,13 +3,19 @@ package com.nicos.datastoresetup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.nicos.datastoresetup.ui.theme.DataStoreSetupTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainView()
                 }
             }
         }
@@ -30,17 +36,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainView() {
+    Box(contentAlignment = Alignment.Center) {
+        Column {
+            Button(
+                onClick = {},
+                modifier = Modifier.width(170.dp)
+            ) {
+                Text("Save String Value")
+            }
+            Button(
+                onClick = {},
+                modifier = Modifier.width(170.dp)
+            ) {
+                Text("Save Specific Value")
+            }
+            Button(
+                onClick = {},
+                modifier = Modifier.width(170.dp)
+            ) {
+                Text("Save All Values")
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    DataStoreSetupTheme {
-        Greeting("Android")
-    }
+@Preview
+fun MainViewPreview() {
+    MainView()
 }
